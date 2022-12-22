0afa5eff938e239f0d2eb71de0a568cdbfac0fc9
gituserpr707 <prashantanagdi1999@gmail.com>
2022-12-22 18:08:42 +0530

    Calculator project file

---
+++ src/com/Calulator/Calculator.java
@@ -0,0 +1,12 @@
+package com.Calulator;
+
+public class Calculator {
+	public static void main(String args[])
+	{
+		Addition Add=new Addition();
+		Add.add(4, 5);
+		Substraction sub=new Substraction();
+		sub.addition(8, 9);
+	}
+
+}
---
+++ src/com/Calulator/Substraction.java
@@ -0,0 +1,10 @@
+package com.Calulator;
+
+public class Substraction {
+
+	public void addition(int a,int b)
+	{
+		int c=a+b;
+	
+	}
+}
